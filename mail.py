b7797ba97faa59da96b1381a3d3c4aa61af1cc57
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-09-23 18:40:32 +1200

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import smtplib
+
 from email.mime.text import MIMEText
 #设置服务器所需信息
 #163邮箱服务器地址
@@ -8,7 +9,7 @@ mail_host = 'smtp.163.com'
 #163用户名
 mail_user = 'monitor383@163.com'  
 #密码(部分邮箱为授权码) 
-mail_pass = 'itproject383*'   
+mail_pass = '123456'   
 #邮件发送方邮箱地址
 sender = 'monitor383@163.com'  
 #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
