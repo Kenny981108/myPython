5be38cda479d4233106954ff17d687c0aa8b3c6c
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-09-23 17:53:20 +1200

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,44 +1,22 @@
-
 #!/usr/bin/env python
-import smtplib
-import sys
-import email.mime.text
 
-# my test mail
-mail_username = 'xincancui9@gmail.com'
-mail_password = 'cxc858019*'
-from_addr = mail_username
-to_addrs = ('1090265975@qq.com')
-
-# HOST & PORT
-HOST = 'smtp.gmail.com'
-PORT = 25
-
-# Create SMTP Object
-smtp = smtplib.SMTP()
-print 'connecting ...'
-
-# show the debug log
-smtp.set_debuglevel(1)
-
-# connet
-try:
-    print smtp.connect(HOST, PORT)
-except:
-    print 'CONNECT ERROR ****'
-# gmail uses ssl
-smtp.starttls()
-# login with username & password
-try:
-    print 'loginning ...'
-    smtp.login(mail_username, mail_password)
-except:
-    print 'LOGIN ERROR ****'
-# fill content with MIMEText's object
-msg = email.mime.text.MIMEText('Hi ,I am leehark')
-msg['From'] = from_addr
-msg['To'] = ';'.join(to_addrs)
-msg['Subject'] = 'hello , today is a special day'
-print msg.as_string()
-smtp.sendmail(from_addr, to_addrs, msg.as_string())
-smtp.quit()
+# your Gmail account  
+import smtplib 
+  
+# creates SMTP session 
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+# start TLS for security 
+s.starttls() 
+  
+# Authentication 
+s.login("xincancui9@gmail.com", "cxc858019*") 
+  
+# message to be sent 
+message = "Message_you_need_to_send"
+  
+# sending the mail 
+s.sendmail("xincancui9@gmai.com", "1090265975@qq.com", message) 
+  
+# terminating the session 
+s.quit() 
