db11e3b6137f05cb349d5cd5609d6ab684f02419
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-09-25 16:27:44 +1200

    Update m.py

--- m.py
+++ m.py
@@ -8,9 +8,7 @@ import pexpect
 
 process = pexpect.spawn("aws configure")
 process.expect("AWS Access Key ID [None]:")
-process.sendline("AKIAIP3LGYHOIKPQ55YQ")
 process.expect("AWS Secret Access Key [None]:")
-process.sendline("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")
 process.expect("Default region name [None]:")
 process.sendline("us-east-1")
 process.expect("Default output format [None]:")
