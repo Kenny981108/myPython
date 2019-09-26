5cacdddd618f3b2281095162918fe580e7c2409a
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-09-26 16:04:04 +1200

    Update m.py

--- m.py
+++ m.py
@@ -4,17 +4,6 @@ import os
 os.system("sudo apt-get -y install python3-pexpect")
 os.system("sudo apt-get -y install awscli")
 
-import pexpect
-
-process = pexpect.spawn("aws configure")
-process.expect("AWS Access Key ID [None]:")
-process.expect("AWS Secret Access Key [None]:")
-process.expect("Default region name [None]:")
-process.sendline("us-east-1")
-process.expect("Default output format [None]:")
-process.sendline("text")
-
-
 p = os.popen("aws sns create-topic --name my-topic")
 fid = p.read()
 fid = fid[22:34]
