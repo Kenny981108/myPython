3c261090c12be86942957863773d941f12079d00
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-09-29 16:57:59 +1300

    Update m.py

--- m.py
+++ m.py
@@ -10,13 +10,14 @@ fid = fid[22:34]
 ec2 = os.popen("ec2metadata --instance-id")
 ec2id = ec2.read()
 ec2id = ec2id.strip('\n')
-if os.system("sudo touch /home/ubuntu/myPython/%s.txt"%(ec2id)):
+fa = os.popen('test -f /home/ubuntu/myPython/%s.txt && echo "1" || echo "0"'%(ec2id))
+fa = fa.read()
+fa.strip()
+if fa == "0"
     os.system("aws sns subscribe --topic-arn arn:aws:sns:us-east-1:%s:my-topic --protocol email --notification-endpoint 1090265975@qq.com"%(fid))
     os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description 'Alarm when CPU exceeds 70%s' --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions arn:aws:sns:us-east-1:%s:my-topic  --unit Percent"%("%",ec2id,fid))
     #print(ec2id)
     os.system("aws cloudwatch put-metric-alarm --alarm-name ebs-mon --alarm-description 'Alarm when EBS volume exceeds 100MB throughput' --metric-name VolumeReadBytes --namespace AWS/EBS --statistic Average --period 300 --threshold 100000000 --comparison-operator GreaterThanThreshold --dimensions Name=VolumeId,Value=%s --evaluation-periods 3 --alarm-actions arn:aws:sns:us-east-1:%s:my-topic "%(ec2id,fid))
-    fa = open('/home/ubuntu/myPython/%s.txt'%(ec2id),'w')
-    fa.write("xxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    fa.close()
+    os.system("sudo touch /home/ubuntu/myPython/%s.txt"%(ec2id))
 else:
     print("well done")
