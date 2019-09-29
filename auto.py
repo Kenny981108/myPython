5b05c796f41d6a2d9b21ba23ed501f57c7f3c98f
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-09-29 19:03:39 +1300

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,20 @@
+#!/user/bin/env python
+
+#change IP
+file = open('/lib/systemd/system/rc.local.service','r')
+lines = []
+for line in file:            
+    lines.append(line)
+file.close()
+lines.insert(24, '[Install]\nWantedBy=multi-user.target\nAlias=rc-local.service')
+s = ''.join(lines)
+file = open('/lib/systemd/system/rc.local.service', 'w+')
+file.write(s)
+file.close()
+del lines[:]
+os.system("sudo ln -s /lib/systemd/system/rc.local.service /etc/systemd/system/rc.local.service")
+os.system("sudo touch /etc/rc.local")
+os.system("sudo chmod +x /etc/rc.local")
+fa = open('/etc/rc.local','w')
+fa.write("#!/bin/bash\npython3 /home/ubuntu/myPython/changeIP.py\npython3 /home/ubuntu/myPython/m.py")
+fa.close()
