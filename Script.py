618b977a5cf908f98e5f5883b919d3fd52f0174e
Kenny981108 <53840820+Kenny981108@users.noreply.github.com>
2019-10-02 17:14:16 +1300

    Update Script.py

--- Script.py
+++ Script.py
@@ -126,25 +126,18 @@ fa.close()
 # #setup net2ftp
 # os.system("sudo apt install unzip")
 # os.system("sudo unzip net2ftp_v1.3.zip -d /var/www/html")
-
-
 os.system("aws configure")
 os.system("aws sns create-topic --name my-topic")
-p = os.popen("aws sns create-topic --name my-topic")
-fid = p.read()
-fid = fid[22:34]
-ec2 = os.popen("ec2metadata --instance-id")
-ec2id = ec2.read()
-ec2id = ec2id.strip('\n')
-os.system("sudo touch /home/ubuntu/myPython/topic.txt")
-fa = open('sudo touch /home/ubuntu/myPython/topic.txt','w')
-fa.write("%s"%(fid))
-fa.close()
-os.system("aws sns subscribe --topic-arn arn:aws:sns:us-east-1:%s:my-topic --protocol email --notification-endpoint 1090265975@qq.com"%(fid))
-os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description 'Alarm when CPU exceeds 70%s' --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions arn:aws:sns:us-east-1:%s:my-topic  --unit Percent"%("%",ec2id,fid))
-#print(ec2id)
-os.system("aws cloudwatch put-metric-alarm --alarm-name ebs-mon --alarm-description 'Alarm when EBS volume exceeds 100MB throughput' --metric-name VolumeReadBytes --namespace AWS/EBS --statistic Average --period 300 --threshold 100000000 --comparison-operator GreaterThanThreshold --dimensions Name=VolumeId,Value=%s --evaluation-periods 3 --alarm-actions arn:aws:sns:us-east-1:%s:my-topic "%(ec2id,fid))
-
-
-
+p2 = os.popen("ec2metadata --instance-id")
+ec2ID = p2.read().strip()
+p1 = os.popen("aws sns create-topic --name alarm")
+arn = p1.read().strip()
+email = "1090265975@qq.com"
+os.system("aws sns subscribe --topic-arn %s --protocol email --notification-endpoint %s"%(arn,email)) # subscribe
+os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-mon+%s --alarm-description 'Alarm when CPU exceeds 70 percent' --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
+os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-CreditUsage+%s --alarm-description 'Alarm when CPU Credit Usage exceeds 70 percent' --metric-name CPUCreditUsage --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
+os.system("aws cloudwatch put-metric-alarm --alarm-name networkIn+%s --alarm-description 'Alarm when NetWorkIn exceeds 70 percent' --metric-name NetworkIn --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
+os.system("aws cloudwatch put-metric-alarm --alarm-name networkOut+%s --alarm-description 'Alarm when NetWorkOut exceeds 70 percent' --metric-name NetworkOut --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
+os.system("aws cloudwatch put-metric-alarm --alarm-name DiskReadBytes+%s --alarm-description 'Alarm when DiskReadBytes exceeds 100MB' --metric-name DiskReadBytes --namespace AWS/EC2 --statistic Average --period 300 --threshold 100000000 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
+os.system("aws cloudwatch put-metric-alarm --alarm-name DiskWriteBytes+%s --alarm-description 'Alarm when DiskWriteBytes exceeds 100MB' --metric-name DiskWriteBytes --namespace AWS/EC2 --statistic Average --period 300 --threshold 100000000 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
 
